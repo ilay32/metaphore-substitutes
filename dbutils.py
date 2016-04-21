74d5f1d6e28b34dbba3ebcbe97f179770ac115ff
ilay <silayster@gmail.com>
2016-04-21 21:26:41 +0300

    basic algorithm, synonyms from wordnet, vectors from LSA-ENG

--- dbutils.py
+++ dbutils.py
@@ -1,4 +1,4 @@
-import pandas,sys,os,json,pymssql
+import pandas,sys,os,json,pymssql,math,copy
 import numpy as np
 from scipy.spatial.distance import cosine
 from MetaphorResearch.DAL_AbstractionDB.DbAccess import ServerConnection
@@ -25,6 +25,11 @@ class SingleWordData:
         self.table = dict()
         self.notfound = set()
     
+    def searchval(self,val):
+        for w,v in self.table.items():
+            if v == val:
+                return w
+
     def has(self,word):
         if word not in self.notfound:
             self.get(word)
@@ -34,9 +39,11 @@ class SingleWordData:
         return False
     
     def empty(obj):
+        if not obj:
+            return True
         if isinstance(obj,int) or  isinstance(obj,float):
             return obj == 0
-        return len(obj) == 0
+        return len(obj) ==  0
 
     def get(self,word):
         if word in self.notfound:
@@ -49,6 +56,7 @@ class SingleWordData:
             if not SingleWordData.empty(qr):
                 self.table[word] = qr
         return qr
+    
     def __str__(self):
         return self.table[:min(len(self.table),5)]
 
@@ -57,30 +65,66 @@ class Abst(SingleWordData):
         return "SELECT ABSTRACT_SCALE FROM PHRASE_ABSTRACT WHERE PHRASE='{0}'".format(word.lower())
 
     def handlequery(self,q):
-        f = q.fetchall()
-        if not q or len(f) == 0:
+        try: 
+            f = q.fetchall()
+            if not q or len(f) == 0:
+                return 0
+            return f[0][0]
+        except:
             return 0
-        return f[0][0]
 
 class Vecs(SingleWordData):
     def queryscheme(self,word):
         return "GetRows '{0}'".format(word.lower())
 
     def handlequery(self,query):
-        ret = []
+        ret = dict()
         for row in query:
-            ret.append(row[3])
-        return np.array(ret)
+            ret.update({row[2] : row[3]})
+        return ret
    
-    # these methods expect np arrays
-    def distance(self,u,v):
-        m = min(len(u),len(v))
-        return cosine(u[:m],v[:m])
+    def distance(u,v):
+        norms = Vecs.norm(u) * Vecs.norm(v)
+        if norms == 0:
+            return -1
+        return Vecs.dot(u,v)/norms
+       # m = min(len(u),len(v))
+       # return cosine(u[:m],v[:m])
+    def dot(u,v):
+        d = 0
+        for colid,uval,vval in Vecs.vectorpair(u,v):
+            d += uval*vval
+        if d == 0:
+            print("zero norm")
+        return d
     
-    def addition(self,u,v):
-        m = min(len(u),len(v))
-        x = u[:m] + v[:m]
-        return x
+    def word_distance(self,w1,w2):
+        if self.has(w1) and self.has(w2):
+            return Vecs.distance(self.get(w1),self.get(w2))
+        return -1
+
+    def vectorpair(u,v):
+        for col_id in u.keys():
+            if col_id in v:
+                yield(col_id,u[col_id],v[col_id])
+            else:
+                yield(col_id,u[col_id],0)
+    
+    # u and v are not of equal length, so this norm is conditioned on shared coordinates (just like the inner product) 
+    def norm(u):
+        return math.sqrt(Vecs.dot(u,u))
+
+    def addition(u,v):
+        s = dict()
+        for colid,uval,vval in Vecs.vectorpair(u,v):
+            s.update({colid:uval+vval})
+        return s
+    
+    def multiply(u,scalar):
+        ret =  copy.deepcopy(u)
+        for k in ret.keys():
+            ret[k] = u[k]*scalar
+        return ret
 
 class Lex(SingleWordData):
     
@@ -92,9 +136,4 @@ class Lex(SingleWordData):
     
     def queryscheme(self,word):
         return "SELECT DISTINCT posType FROM Lexicon WHERE word='{0}' AND lemma = '{0}'".format(word)
-def explore(query,catalog):
-    specs = json.load(open(os.path.join(os.getcwd(),'MetaphorResearch','user.spec')))
-    db = DB(specs,catalog) 
-    ans = db.query(query)
-    print(ans)
-    return
+
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 # main frame of the program #
-import math
+import math,pandas,sys
 import numpy as np
+from nltk.corpus import wordnet as wn
+from pairs import pairs
 from dbutils import *
 vecs = Vecs(DB('LSA-ENG'))
 Ngrams = DB('NgramsCOCAAS')
 abst = Abst(Ngrams)
 lex = Lex(DB('NgramsCOCAAS'))
+
 class MetaphorSubstitute:
     def __init__ (self,options):
+        print("initializing "+options['verb']+", "+options['obj'])
         for key in options.keys():
             self.__dict__[key] = options[key]
         self.instance_centroid = self.get_instance_centroid()
         self.substitutes = list()
+        self.go = True
+        if SingleWordData.empty(self.instance_centroid):
+            self.go = False
     
     def __str__(self):
         return "verb:"+self.verb+", object:"+self.obj;
@@ -20,6 +27,7 @@ class MetaphorSubstitute:
     # in this version,simple filtering by the instance abstractness
     # threshold
     def object_cluster(self,verb):
+        print("fetching objects set for "+verb)
         clust = list()
         q =  Ngrams.query("GetNgrams '{0}',{1},{2},{3},{4},{5}".format(
             verb,
@@ -33,49 +41,78 @@ class MetaphorSubstitute:
             print("error fetching object cluster")
             return
         added = 0
-        rows = q.fetchall()
-        while added < min(self.object_cluster_size,len(rows) - 1):
-            lem = rows[added][10] # take the lemma in this case ?
-            clust.append(lem)
-            added += 1
+        cur = 0
+        try:
+            rows = q.fetchall()
+            while added < min(self.object_cluster_size,len(rows) - 1):
+                lem = rows[cur][10] # take the lemma in this case ?
+                if(vecs.has(lem)):
+                    clust.append(lem)
+                    added += 1
+                cur += 1
+        except:
+            print(verb+" can't find noun cluster. ignored")
         return clust
 
     def cluster_centroid(self,cluster):
+        print("computing cluster centroid for ",cluster[:5],"...")
         added = 0
-        l = len(vecs.get('queen')) # len of the vector is 300
-        acc = np.zeros(l)
-        # just sum the cluster words' vectors to get a semantic
-        for word in cluster:
+        acc = vecs.get(cluster[0])
+        for word in cluster[1:]:
             if vecs.has(word):
-                if math.isnan(vecs.get(word)[0]): 
+                if SingleWordData.empty(vecs.get(word)): 
                     print("nan in vector", word)
-                acc = vecs.addition(acc,vecs.get(word))
+                acc = Vecs.addition(acc,vecs.get(word))
                 added += 1
-        return acc/added
+        return Vecs.multiply(acc,1/added)
     
     def get_instance_centroid(self):
+        print("computing instance centroid")
         raw_cluster = self.object_cluster(self.verb)
-        return vecs.addition(self.cluster_centroid(raw_cluster),(vecs.get(self.obj)*self.instance_object_weight))/2
+        if SingleWordData.empty(raw_cluster):
+            print("this instance has no cluster. calling it quits")
+            return None 
+        cent = self.cluster_centroid(raw_cluster)
+        return Vecs.multiply(Vecs.addition(cent,Vecs.multiply(vecs.get(self.obj),self.instance_object_weight)),1/2)
     
     # this is probably all wrong    
+    #def verb_synonyms(self):
+    #    identifier = self.obj+"_"+self.verb+"\t"
+    #    with open(self.synonyms_file) as s:
+    #        for line in s:
+    #            if identifier in line:
+    #                synraw = line.partition(identifier)[2].split(',')
+    #                print(synraw[:15])
+    #                synfiltered = [w for w in synraw if lex.has(w) and 2 in lex.get(w) and abst.has(w) and abst.get(w) > self.abstract_thresh]
+    #                print("syns:\n", )
+    #                return synfiltered[:min(self.number_of_synonyms,len(synfiltered))]
+    #    return None
     def verb_synonyms(self):
-        identifier = self.obj+"_"+self.verb+"\t"
-        with open(self.synonyms_file) as s:
-            for line in s:
-                if identifier in line:
-                    synraw = line.partition(identifier)[2].split(',')
-                    print(synraw[:15])
-                    synfiltered = [w for w in synraw if lex.has(w) and 2 in lex.get(w) and abst.has(w) and abst.get(w) > self.abstract_thresh]
-                    print("syns:\n", )
-                    return synfiltered[:min(self.number_of_synonyms,len(synfiltered))]
-        return None
-    
-    def cluster_distance(self,verb):
-        return vecs.distance(self.cluster_centroid(self.object_cluster(verb)),self.instance_centroid)
+        print("getting synonyms:")
+        synsets = wn.synsets(self.verb)
+        syn = list()
+        # make sure they're verbs
+        synsets = [s for s in synsets if ".v." in s.name()]
+        # loop once to get heypernym sets
+        for s in synsets:
+            synsets += s.hypernyms()
+        # loop again to collect lemmas
+        for s in synsets:
+            syn += [l.name() for l in s.lemmas() if l.name() != self.verb]
+        print("found",syn[:5],"...")
+        return list(set(syn[:min(self.number_of_synonyms,len(syn))]))
+
+    def cluster_distance(self,objects_cluster):
+        return Vecs.distance(self.cluster_centroid(objects_cluster),self.instance_centroid)
 
     def find_substitutes(self):
         syn = self.verb_synonyms()
-        subs  = sorted([(s,self.cluster_distance(s)) for s in syn])
+        subs = list()
+        for verb in syn:
+            vc = self.object_cluster(verb)
+            if not SingleWordData.empty(vc):
+                subs.append((verb,self.cluster_distance(vc)))
+            subs = sorted(subs)
         self.substitutes = subs
         return subs 
     
@@ -89,19 +126,33 @@ class MetaphorSubstitute:
 if __name__ ==  '__main__':
     #v = input("verb:") 
     #o = input("object:")
-    ms = MetaphorSubstitute({
-        "verb": 'shape',
-        "obj":  'result',
-        "object_cluster_size" : 10,
-        "instance_object_weight": 5,
-        "synonym_draw" : 10,
-        "synonyms_file" : 'synonyms.txt',
-        "search_left" : 0,
-        "search_right": 5,
-        "abstract_thresh" : 0.6,
-        "min_MI" : 0.5,
-        "number_of_synonyms" : 15
-    })
-     
-    #ms.find_substitutes()
-    #print("finally:\n",ms.substitutes)
+    limit = sys.argv[1]
+    if not limit:
+        limit = len(pairs)
+    rundata = list()
+    for i in range(int(limit)):
+        o,v = pairs[i]
+        ms = MetaphorSubstitute({
+            "verb": v,
+            "obj":  o,
+            "object_cluster_size" : 10,
+            "instance_object_weight": 5,
+            "synonym_draw" : 10,
+            "synonyms_file" : 'synonyms.txt',
+            "search_left" : 0,
+            "search_right": 5,
+            "abstract_thresh" : 0.6,
+            "min_MI" : 0.1,
+            "number_of_synonyms" : 15
+        })
+        if ms.go:
+            subs = ms.find_substitutes()
+            d = {"pair": v +"(v), "+o+ "(n)", "substitutes" : subs}
+            rundata.append(d)
+        print(ms," done","\n====================\n")
+    rundata = pandas.DataFrame(rundata)
+    print("\n\t\tHura!\n")
+
+
+
+
