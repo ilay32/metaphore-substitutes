8176c309d10146a5ac6a8aac6fc909b9fd7efcf3
ilay <silayster@gmail.com>
2017-01-21 22:25:33 +0200

    Revive Neuman's experiment 2 as a MetaphorSubstitute subclass\n\tfor rhetoric purposes

--- rundata.py
+++ rundata.py
@@ -1,6 +1,7 @@
 import os,pandas,time
 from dbutils import RunData,printlist
-def explore_rundata(num=5):
+
+def data_files(num):
     files = os.listdir(RunData.datadir)
     paths = list()
     ignored = list()
@@ -14,17 +15,28 @@ def explore_rundata(num=5):
         print("ignored:")
         printlist(ignored)
     paths.sort(key=lambda x: x[1],reverse=True)
-    if not num:
+    if num > len(paths):
         num = len(paths) - 1
-    for i,p in enumerate(paths[:num],1):
+    return paths[:num]
+
+def explore_rundata(num=5):
+    paths = data_files(num)
+    for i,p in enumerate(paths,1):
         print(str(i)+".",time.ctime(p[1]))
     choice = input("choose data to load: ")
     data = pandas.read_pickle(paths[int(choice) - 1][0])
     data.when()
     data.how()
     print("\nprint data.show() to view the results of this run")
-    print("print x = explore_rundata(): to load another run as x")
     return data
 
+def list_summary(num=10):
+    paths = data_files(num)
+    for p in paths:
+        d = pandas.read_pickle(p[0])
+        d.when()
+        print(d)
+
 if __name__ == '__main__':
-    data = explore_rundata()
+    print("print data = explore_rundata(): to load a run as data")
+    print("print list_summary(n) for a summary of n runs back")
